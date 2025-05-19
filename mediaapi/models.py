7e0e996079d37d8bc2f7afd0eb7e6e8d0e958dab
ashraf <ashrafchekintakath@gmail.com>
2025-05-19 17:51:17 +0530

    init

--- mediaapi/models.py
+++ mediaapi/models.py
@@ -1,3 +1,13 @@
+
 from django.db import models
 
-# Create your models here.
+class MediaFile(models.Model):
+    original_filename = models.CharField(max_length=255)
+    file_name = models.CharField(max_length=255)
+    cdn_url = models.URLField()
+    is_private = models.BooleanField(default=False)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.original_filename
+
